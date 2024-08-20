be421463760e652af9230b8d97c17ee0a8ccbf96
Carmen Pareja <cparejaruiz@gmail.com>
2024-08-20 14:41:18 +0200

    toupper

---
+++ ft_toupper.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <unistd.h>
+
+int    ft_toupper(int ch)
+{
+    if (ch >= 'a' && ch <= 'z')
+    {
+        ch = ch - 32;
+    }
+    else
+    {
+        ch;
+    }
+
+    return ch;
+}
+
+
+int main(void)
+{
+
+    int c =  'Z';
+    int newChar = ft_toupper(c);
+     write(1,&c,1);
+     write(1,"\n",1);
+     write(1,&newChar,1);
+     write(1,"\n",1);
+
+
+    return 0;
+    }
