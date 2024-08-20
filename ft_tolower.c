ed0dab62dd8f9bb741c0ff9393c9cf6470cc0f01
Carmen Pareja <cparejaruiz@gmail.com>
2024-08-20 14:53:35 +0200

    tolower

---
+++ ft_tolower.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <unistd.h>
+
+int    ft_toupper(int ch)
+{
+    if (ch >= 'A' && ch <= 'Z')
+    {
+        ch = ch + 32;
+    }
+    else
+    {
+        ch;
+    }
+
+    return ch;
+}
+
+
+int main(void)
+{
+
+    int c =  '2';
+    int newChar = ft_toupper(c);
+     write(1,&c,1);
+     write(1,"\n",1);
+     write(1,&newChar,1);
+     write(1,"\n",1);
+
+
+    return 0;
+    }
