56847372688e389943de639af00ae8bbfc46b4d3
cparejaruiz <cparejaruiz@gmail.com>
2024-10-16 00:16:04 +0200

    modify format

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -1,15 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: carmen <carmen@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/16 00:13:42 by carmen            #+#    #+#             */
+/*   Updated: 2024/10/16 00:15:47 by carmen           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-void ft_putstr_fd(char *s, int fd)
+void	ft_putstr_fd(char *s, int fd)
 {
-    int i = 0;
-    if (!s)  
-        return ;
-    while(s[i]!='\0')
-    {
-        ft_putchar_fd(s[i],fd);
-        i++;
-    }
+	int	i;
+
+	i = 0;
+	if (!s)
+		return ;
+	while (s[i] != '\0')
+	{
+		ft_putchar_fd(s[i], fd);
+		i++;
+	}
 }
 /*
 int main() {
