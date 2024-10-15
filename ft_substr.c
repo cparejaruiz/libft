e01d6be86dff3719a047b94cc61e9cf7c3fc35ba
cparejaruiz <cparejaruiz@gmail.com>
2024-10-15 21:23:16 +0200

    ft_substr

---
+++ ft_substr.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: carmen <carmen@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/15 21:21:54 by carmen            #+#    #+#             */
+/*   Updated: 2024/10/15 21:22:02 by carmen           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+    int i = 0;
+    int j = 0;
+    int slen = ft_strlen(s);
+
+    char *string = (char *) malloc ((len +1) * sizeof(char));
+    if (!string)
+    {
+        return NULL;
+    }
+    
+    if (start>=slen)
+    {
+        return strdup("");
+    }
+    
+    while (s[start + i] != '\0' && i <len)
+    {
+        string[j] = s[start+i];
+        i++;
+        j++;
+    }
+    string[j] = '\0';
+    return string;
+}
+
+/*
+int main(void) {
+    char *original = "Hello,u World!";
+    unsigned int start = 2;
+    size_t length = 5;
+
+    char *substring = ft_substr(original, start, length);
+
+    if (substring != NULL) {
+        printf("Subcadena: %s\n", substring);  
+        free(substring);  
+    } else {
+        printf("Error al asignar memoria para la subcadena\n");
+    }
+    return 0;
+}
+*/
+
