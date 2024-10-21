bf3adb7167c47e35a8d96efe7e4b0c26346f8403
cparejaruiz <cparejaruiz@gmail.com>
2024-10-21 21:10:17 +0200

    update incorrect funcion

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,46 +6,46 @@
 /*   By: carmen <carmen@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 11:51:54 by carmen            #+#    #+#             */
-/*   Updated: 2024/10/16 23:14:01 by carmen           ###   ########.fr       */
+/*   Updated: 2024/10/21 21:09:10 by carmen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *str, const char *to_find, size_t len)
+char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	size_t	i;
 	size_t	j;
 
 	i = 0;
 	j = 0;
-	if (!str && len == 0)
+	if (!big && len == 0)
 		return (NULL);
-	if (*to_find == '\0')
-		return ((char *)str);
-	while (i < len && str[i] != '\0')
+	if (*little == '\0')
+		return ((char *)big);
+	while (i < len && big[i] != '\0')
 	{
-		if (str[i] == to_find[0])
+		if (big[i] == little[0])
 		{
 			j = 0;
-			while (i + j < len && str[i + j] == to_find[j]
-				&& to_find[j] != '\0')
+			while (i + j < len && big[i + j] == little[j]
+				&& little[j] != '\0')
 				j++;
-			if (to_find[j] == '\0')
-				return ((char *) &str[i]);
-			i++;
+			if (little[j] == '\0')
+				return ((char *) &big[i]);
 		}
+		i++;
 	}
-return (NULL);
+	return (NULL);
 }
 
 /*
 int main() {
-    const char *largestring = "Foo Bar Baz Uuu";
-    const char *smallstring = "Baz";
+    const char *largebiging = "Foo Bar Baz Uuu";
+    const char *smallbiging = "Baz";
     char *ptr_custom;
 
-    ptr_custom = ft_strnstr(largestring, smallstring, strlen(largestring));
+    ptr_custom = ft_strnstr(largebiging, smallbiging, biglen(largebiging));
 
     if (ptr_custom) {
         printf("ft_strnstr: Subcadena encontrada: %s\n", ptr_custom);
